6af221e27c4751487358ccf09941862d4ce9e283
gaurav agarwal <agarwalgaurav811@gmail.com>
2018-03-21 21:32:47 +0530

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,18 @@
+<?php
+
+/***************************************************************************
+ *                         Connect to MySQL Server                         *
+ ***************************************************************************/
+
+$db=mysqli_connect ("localhost", "root", "gaurav","saDb") or die ('I cannot connect to the database because: ' . mysql_error());
+#mysql_select_db ("saDb");
+//die;
+/////// Update database login details here /////
+$hab_dbost_name = "localhost"; // host name 
+$hab_database = "saDb";       // database name
+$hab_username = "root";            // login userid 
+$hab_password = "gaurav";            // password 
+//////// End of database details of server //////
+
+									
+?>
